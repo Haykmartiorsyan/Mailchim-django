55f8c457b37d83b5eebfc62117919bbe13dd2e40
Hayk <haykmartirosyan21@gmail.com>
2017-11-12 18:44:12 +0400

    First

--- subscribe/urls.py
+++ subscribe/urls.py
@@ -1,10 +1,9 @@
 from django.conf.urls import url
 from django.views.generic.base import TemplateView
-from .views import subscribe, MemberDeleteView
+from .views import subscribe
 
 urlpatterns = (
     url(r'^', subscribe, name='subscribe'),
-    url(r'^(?P<slug>[\w-]+)/delete/$', MemberDeleteView.as_view(), name='delete'),
     url(r'^subscribe/success/$', TemplateView.as_view(
         template_name='mailchimp/successful_subscription.html'),
         name='subscribe_success'),
--- subscribe/views.py
+++ subscribe/views.py
@@ -1,21 +1,15 @@
 from django.core.urlresolvers import reverse
-from django.http import HttpResponseRedirect, HttpResponse
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from .forms import SubscribeForm
-from rest_framework.generics import DestroyAPIView, RetrieveAPIView
 import urllib
 import json
 from django.conf import settings
-from django.views.generic import View
 
 
 key = str(settings.MAILCHIMP_API_KEY)
 list_sub = str(settings.MAILCHIMP_SUBSCRIBE_LIST_ID)
 data_center = str(settings.MAILCHIMP_DATA_CENTER)
-
-url = 'https://'+data_center+'.api.mailchimp.com/3.0/lists/'+list_sub+'/members?apikey='+key+''
-r = urllib.request.urlopen(url)
-data = json.loads(r.read())
 
 
 def subscribe(request):
@@ -29,7 +23,9 @@ def subscribe(request):
     else:
         form = SubscribeForm()
 
-
+    url = 'https://' + data_center + '.api.mailchimp.com/3.0/lists/' + list_sub + '/members?apikey=' + key + ''
+    r = urllib.request.urlopen(url)
+    data = json.loads(r.read())
 
     return render(request, 'mailchimp/subscribe.html', {
         'form': form,
@@ -39,10 +35,6 @@ def subscribe(request):
         'data_center': data_center,
     })
 
-
-class MemberDeleteView(DestroyAPIView):
-    queryset = data
-    lookup_field = 'slug'
 
 
 
